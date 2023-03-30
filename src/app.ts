b8c92eac4bfcdb18bf4ff15d99c7499f91c58095
kimjunhee <developer9101@gmail.com>
2023-03-30 11:18:25 +0900

    template tag class타입 사용하여 표시

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true // 2번째 인자: 하위 태그들도 가져올지 여부
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
