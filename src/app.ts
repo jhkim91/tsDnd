6a91e1a8a3084a45384942233b9db2d133e15c07
kimjunhee <developer9101@gmail.com>
2023-03-30 11:41:30 +0900

    submit event시 input값 출력 테스트

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,46 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
     const importedNode = document.importNode(
       this.templateElement.content,
       true // 2번째 인자: 하위 태그들도 가져올지 여부
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log("title", this.titleInputElement.value);
+    console.log("description", this.descriptionInputElement.value);
+    console.log("people", this.peopleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
