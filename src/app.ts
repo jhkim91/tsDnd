dcfb33080299a0d7b5b9f61870c619f9b9018efc
kimjunhee <developer9101@gmail.com>
2023-04-03 00:48:32 +0900

    namespace import 위치 변경

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,4 @@
 // ts에서 / 3개를 인식함
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
 /// <reference path="components/project-input.ts" />
 /// <reference path="components/project-list.ts" />
 
