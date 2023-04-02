aa94649629d996c843514e9c7fdc299a1b67e315
kimjunhee <developer9101@gmail.com>
2023-04-03 01:08:00 +0900

    namespace -> import 변경(이유: namespace는 한번 호출시 다른파일에 모두 영향이가기에 해당파일에서만 사용되는 import 로 변경)

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,57 +1,54 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../models/project.ts" />
-/// <reference path="../models/drag-drop.ts" />
+import { Draggable } from "../models/drag-drop.js";
+import { Project } from "../models/project.js";
+import { Component } from "./base-component.js";
+import { autobind } from "../decorators/autobind.js";
 
-namespace App {
-  // ProjectItem Class
-  export class ProjectItem
-    extends Component<HTMLUListElement, HTMLLIElement>
-    implements Draggable
-  {
-    private project: Project;
+// ProjectItem Class
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
 
-    get persons() {
-      if (this.project.people === 1) {
-        return "1 person";
-      } else {
-        return `${this.project.people} persons`;
-      }
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
     }
+  }
 
-    constructor(hostId: string, project: Project) {
-      super("single-project", hostId, false, project.id);
-      this.project = project;
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
 
-      this.configure();
-      this.renderContent();
+    this.configure();
+    this.renderContent();
+  }
+
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move"; // 항목을 새 위치로 이동 가능한 옵션
+  }
+
+  dragEndHandler(_: DragEvent) {
+    const listEls = document.querySelectorAll(
+      "#active-projects-list,#finished-projects-list"
+    )!;
+    for (const listEl of listEls) {
+      listEl.classList.remove("droppable");
     }
+  }
 
-    @autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData("text/plain", this.project.id);
-      event.dataTransfer!.effectAllowed = "move"; // 항목을 새 위치로 이동 가능한 옵션
-    }
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
 
-    dragEndHandler(_: DragEvent) {
-      const listEls = document.querySelectorAll(
-        "#active-projects-list,#finished-projects-list"
-      )!;
-      for (const listEl of listEls) {
-        listEl.classList.remove("droppable");
-      }
-    }
-
-    configure() {
-      this.element.addEventListener("dragstart", this.dragStartHandler);
-      this.element.addEventListener("dragend", this.dragEndHandler);
-    }
-
-    renderContent() {
-      this.element.querySelector("h2")!.textContent = this.project.title;
-      this.element.querySelector("h3")!.textContent =
-        this.persons + " assigned";
-      this.element.querySelector("p")!.textContent = this.project.description;
-    }
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,19 +1,17 @@
-namespace App {
-  // autobind decorator
-  export function autobind(
-    //미사용 변수 경고 없애기위해서 _, _순번 규칙을 따르거나 tsconfig.js에서 noUnusedParameters=false로 변경하면됨
-    _: any, // target
-    _2: string, // methodName
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+// autobind decorator
+export function autobind(
+  //미사용 변수 경고 없애기위해서 _, _순번 규칙을 따르거나 tsconfig.js에서 noUnusedParameters=false로 변경하면됨
+  _: any, // target
+  _2: string, // methodName
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
